3e9ee8aaea110e2d94ed2e0b5e62a28d500fcbce
abhishek-nyllabs <abhishek@nyllabs.tech>
2018-03-30 13:29:43 -0400

    added node packages

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Server File Invoked");
+var http = require('http');
+var express = require('express');
+var app = express();
+
+const port = process.env.port || '3000'
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running node app'));
