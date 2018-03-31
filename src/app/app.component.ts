c83b0763c6e66a44d0cf1faaf78346d6ecb79b6e
abhishek-nyllabs <abhishek@nyllabs.tech>
2018-03-30 23:33:38 -0400

    implemented Typed.js typewriter effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit(){
+
+    var typed = new Typed('.hello-text', {
+      strings: ["^1000hello"],
+      typeSpeed: 100,
+      showCursor: false,
+      cursorChar: '|',
+    });  
+
+    var typed = new Typed('.element', {
+      strings: ["^1500 I am Abhishek Kumar."],
+      typeSpeed: 100,
+      showCursor: false,
+    });  
+  }
 }
